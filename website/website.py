b052825a305c784e2a6478b27f3b5c8a802e7446
Jorge Valdes Flores <jorgeignaciovaldesflores@gmail.com>
2025-03-02 23:17:07 -0300

    ejercicios varios con reflex

--- website/website.py
+++ website/website.py
@@ -1,14 +1,30 @@
 import reflex as rx 
 
+class MyState(rx.State):
+    count: int = 0
+    #color : str = "red"
+
+    def increment(self):
+        self.count += 1
+        #self.color = "blue"
+
+def counter():
+    return rx.hstack(
+        rx.heading(MyState.count),
+        #rx.heading("Counter", color=MyState.color),
+        #rx.heading(MyState.count),
+        rx.button("Increment", on_click=MyState.increment),
+    )
 
 def button():
-    return rx.button("Click Me", border_radius="15px", font_size="18px")
+    return rx.button("Click Me", border_radius="15px", font_size="18px", color_scheme="red")
 
 def index():
     return rx.box(
-        rx.text("Hello World!!!"),
-        half_filled_progress(),
-        button(),
+        #rx.text("Hello World!!!"),
+        #half_filled_progress(),
+        #button(),
+        counter(),
     )
 
 def half_filled_progress():
